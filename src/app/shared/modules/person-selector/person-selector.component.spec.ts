e70f1545c1f7c6d2f14826ce2bdb90b8d7ed5a6c
Abdul Baset <abdul.baset@iqvia.com>
2023-09-12 22:39:12 +0600

    add deployment config

---
+++ src/app/shared/modules/person-selector/person-selector.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PersonSelectorComponent } from './person-selector.component';
+
+describe('PersonSelectorComponent', () => {
+  let component: PersonSelectorComponent;
+  let fixture: ComponentFixture<PersonSelectorComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PersonSelectorComponent]
+    });
+    fixture = TestBed.createComponent(PersonSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/modules/person-selector/person-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-selector',
+  templateUrl: './person-selector.component.html',
+  styleUrls: ['./person-selector.component.scss']
+})
+export class PersonSelectorComponent {
+
+}
