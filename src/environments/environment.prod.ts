fcd7bbb4de652052fef6ef7d78591ca83bcba523
Abdul Baset <abdul.baset@iqvia.com>
2023-09-12 10:20:55 +0600

    add production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   applicationName: 'Info Booth',
-  baseUrl: 'https://localhost:8088/api/',
+  baseUrl: 'http://sarwarjahan-001-site1.etempurl.com/api/',
   production: true,
   hostKey: window.location.host.replace(/[:\/]/, '_'),
 };
